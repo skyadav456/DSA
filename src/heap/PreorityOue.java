e907727d16db040b0d674cf02a52573a8b9c5f41
skyadav456 <sharadk4545@gmail.com>
2026-07-19 04:21:27 +0530

    Heap implementation

--- src/heap/PreorityOue.java
+++ src/heap/PreorityOue.java
@@ -1,10 +1,14 @@
 package heap;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class PreorityOue {
     public static void main(String[] args) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        //PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+                Collections.reverseOrder());
         pq.add(50);
         pq.add(20);
         pq.add(70);
